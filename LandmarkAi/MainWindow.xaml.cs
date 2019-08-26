e635c3c8bc9a5d1d6447084f70d02e0a3dab98c6
Memou <equaco@gmail.com>
2019-08-26 18:11:53 -0400

    Api connection completed

--- LandmarkAi/MainWindow.xaml.cs
+++ LandmarkAi/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -36,8 +38,33 @@ namespace LandmarkAi
             {
                 string fileName = dialog.FileName;
                 selectedImage.Source = new BitmapImage(new Uri(fileName));
+
+                MakePredictionAsync(fileName);
             }
 
+        }
+
+        private async void MakePredictionAsync(string fileName)
+        {
+            string url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v3.0/Prediction/7460cfbe-1bda-4d28-8a36-37c8d1346c4f/classify/iterations/Iteration1-Landmarks/image";
+            string prediction_key = "bb23bca66eb34bf58aec4f68c4687636";
+            string content_type = "application/octet-stream";
+            var file = File.ReadAllBytes(fileName);
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Prediction-Key", prediction_key);
+                using(var content = new ByteArrayContent(file))
+                {
+                    content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(content_type);
+                    var response = await client.PostAsync(url,content);
+
+                    var responseString = await response.Content.ReadAsStringAsync();
+
+
+                }
+
+            }
         }
     }
 }
